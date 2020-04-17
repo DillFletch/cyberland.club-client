d84a52de7754d93ce9b124647977681167491132
Joe <joe@joeth.uk>
2020-04-17 10:00:11 +0100

    whoopsy, forgot to git add

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-## Cyberland Python Client
+# Cyberland Python Client
 
 This client was written originally for https://cyberland.club, but eventually that died, so it is now still being developed for all of the forks and re-writes
 
-# Screenshots:
-![alt text](https://i.imgur.com/ncsXCNp.png)
+## Dependencies
+This program depends on:
+
+* Python 3
+* Python 3 Requests
+* Python 3 json
+
+To install these dependencies: 
+
+`pip install -r requirements.txt`
+
+I would recommend making a python3 virtual environment before you install the dependencies:
+
+`python3 -m venv venv`
+
+and to activate the virtual environment:
+
+`source venv/bin/activate`
+
+Make sure you activate the virtual envirnment every time before you run the program, if it is not activated already.
+
+## Usage
+`python3 client.py url`
+
+url must be a valid URL. 
+
+Defaults to https://cyberland.digital 
+
+
+## Screenshots:
+![Client showing an ANSI image](https://i.imgur.com/ncsXCNp.png)
+![Client showing posts on a news board](https://i.imgur.com/biQa8g3.jpg)
--- client.py
+++ client.py
@@ -1,37 +1,30 @@
 import os, json, requests, sys
 
-## Check is user has supplied instance URL as an arguement, default to https://cyberland.digital
-try:
-    if type(sys.argv[1]) is str:
-        url = sys.argv[1]
-except:
-    url = "https://cyberland.digital"
-
-
-## Get and print relevant ASCII art from top 8 lines of homepage
-for line in requests.get(url).text.split("\n")[:8]:
-    print(line)
-
-refreshed = False
-boardSelection = "nill"
-
-## Initial selection
-print("sm0lman's cringe cyberland.club python client\n")
-print("Please select the board you wish to view")
-print("/t/")
-print("/o/")
-print("/n/")
-print("/i/") ## Will be updated in the future to fetch boards.json and show available boards on instance
-boardSelection = input("Selection: ")
+def chooseBoard():
+    print("Please select the board you wish to view")
+    print("/t/")
+    print("/o/")
+    print("/n/")
+    print("/i/") ## Will be updated in the future to fetch boards.json and show available boards on instance when implemented into cyberland protocol
+    return input("Selection: ")
 
 ## fetch OP posts
-def boardFetch(board):
+def OPFetch(board):
     print("Fetching ",board," OP posts")
     content = requests.get(url+board+"?thread=0").content
     showPosts(content)
+    return showMenu('board')
+
+## Fetch posts from a thread
+def threadFetch(board, thread):
+    print("Fetching replies to post "+thread+" on board "+board)
+    content = requests.get(url+board+"?thread="+thread).content
+    showPosts(content)
+    return showMenu('thread')
 
 ## Show posts nicely formatted
 def showPosts(content):
+    clearScreen()
     posts = json.loads(content)
     posts = list(reversed(posts))
     for post in range(1,len(posts)):
@@ -54,63 +47,86 @@ def showMenu(type):
     menuChoice = str(input("Select a choice: "))
     return menuChoice
 
-
-while True:
-    if refreshed == False: ## Check if it is refreshed
-       boardFetch(boardSelection)
-    refreshed = False
-    menuChoice = showMenu("board")
-
-    if menuChoice.lower() == "n": ## New OP post
-        message = input("What message?: ")
-        r = requests.post(url+boardSelection, data={"content":message, "replyTo": "0"})
-        showPosts(r.content)
-        refreshed = True
-        print(r.status_code)
-
-    elif menuChoice.lower() == "f": ## Follow thread
-        threadNumber = input("Please choose a thread to view: ")
-        os.system('clear')
-        content = requests.get(url+boardSelection+"?thread="+threadNumber+"&num=50").content
-        showPosts(content)
-        showMenu("thread")
-
-
-    elif menuChoice.lower() == "r": ## Reply to thread
-        replyTo = input("Please choose a thread number to reply to: ")
-        message = input("Please choose a message: ")
-        r = requests.post(url+boardSelection, data={"content":message,"replyTo":replyTo})
-        print(r.status_code)
-
-    elif menuChoice.lower() == "b": ## Refresh board
-        os.system("clear")
-        boardFetch(boardSelection)
-        refreshed = True
-
-    elif menuChoice.lower() == "t": ## Refresh thread
-        os.system('clear')
-        content = requests.get(url+boardSelection+"?thread="+threadNumber+"&num=50").content
-        showPosts(content)
-
-    elif menuChoice.lower() == "c": ## Change board
-        print("Boards to change to: ")
-        print("/t/")
-        print("/o/")
-        print("/n/")
-        print("/i/")
-        boardSelection = input("Selection: ")
-
-    elif menuChoice.lower() == "s": ## Send ASCII
-        ansFileName = input("Please give the name of the ANSI file you want to send: ")
-        print("Sending ANSI file...")
-        ansiFile = open(ansFileName, "r")
-        message = input("Insert a message to put alongside your image: ")
-        r = requests.post(url+boardSelection, data={"content":(message + "\n" + ansiFile.read())})
-        print(r.status_code)
-        
-    elif menuChoice.lower() == "q": ## Quit
-        print("Quitting! Bye Bye...")
-        quit()
-        
+## Clears the screen, with a different method based on which OS is being run.
+def clearScreen():
+    if sys.platform == "win32":
+        os.system("cls")
     else:
-      print("An error occured...")
+        os.system("clear")
+
+
+if __name__ == "__main__":
+   
+    ## Check is user has supplied instance URL as an arguement, default to https://cyberland.digital
+    try:
+        if type(sys.argv[1]) is str:
+            url = sys.argv[1]
+    except:
+        url = "https://cyberland.digital"
+
+
+    ## Get and print relevant ASCII art from top 8 lines of homepage
+    for line in requests.get(url).text.split("\n")[:8]:
+        print(line)
+
+    refreshed = False
+
+    ## Initial selection
+    print("sm0lman's cyberland python client\n")
+    boardSelection = chooseBoard()
+    menuChoice = OPFetch(boardSelection)
+    refreshed = True
+
+    while True:
+        if refreshed == False: ## Check if it is refreshed
+            menuChoice = OPFetch(boardSelection)
+        refreshed = False
+
+        if menuChoice.lower() == "n": ## New OP post
+            message = input("What message?: ")
+            r = requests.post(url+boardSelection, data={"content":message, "replyTo": "0"})
+            showPosts(r.content)
+            refreshed = True
+            print(r.status_code)
+
+        elif menuChoice.lower() == "f": ## Follow thread
+            threadNumber = input("Please choose a thread to view: ")
+            menuChoice = threadFetch(boardSelection, threadNumber)
+            refreshed = True
+
+
+        elif menuChoice.lower() == "r": ## Reply to thread
+            replyTo = input("Please choose a thread number to reply to: ")
+            message = input("Please choose a message: ")
+            r = requests.post(url+boardSelection, data={"content":message,"replyTo":replyTo})
+            print(r.status_code)
+            menuChoice = threadFetch(boardSelection, replyTo)
+            refreshed = True
+
+        elif menuChoice.lower() == "b": ## Refresh board
+            clearScreen()
+            menuChoice = OPFetch(boardSelection)
+            refreshed = True
+
+        elif menuChoice.lower() == "t": ## Refresh thread
+            clearScreen()
+            menuChoice = threadFetch(boardSelection, threadNumber)
+            refreshed = True
+
+        elif menuChoice.lower() == "c": ## Change board
+            boardSelection = chooseBoard()
+
+        elif menuChoice.lower() == "s": ## Send ASCII
+            ansFileName = input("Please give the name of the ANSI file you want to send: ")
+            print("Sending ANSI file...")
+            ansiFile = open(ansFileName, "r")
+            message = input("Insert a message to put alongside your image: ")
+            r = requests.post(url+boardSelection, data={"content":(message + "\n" + ansiFile.read())})
+            print(r.status_code)
+            
+        elif menuChoice.lower() == "q": ## Quit
+            print("Quitting! Bye Bye...")
+            quit()
+            
+        else:
+            print("An error occured...")
